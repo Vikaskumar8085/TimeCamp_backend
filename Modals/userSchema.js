2fc239a097eb2af72ccfef47659e01369731a723
Vikaskumar8085 <vikaskumar7049002404@gmail.com>
2024-08-06 19:24:06 +0530

    update code

--- Modals/userSchema.js
+++ Modals/userSchema.js
@@ -15,7 +15,7 @@ const UserSchema = mongoose.Schema({
     trim: true,
     match: [
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      "Please enter a valid emaial",
+      "Please enter a valid email",
     ],
     required: [true, "Please enter your  email"],
   },
