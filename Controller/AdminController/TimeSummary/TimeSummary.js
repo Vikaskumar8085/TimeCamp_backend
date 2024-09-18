3eb90610e37f73c121c921e531bebe79d39de905
ayush <ayush@ayush.ayush>
2024-09-18 18:04:31 +0530

    18Sep2024

---
+++ Controller/AdminController/TimeSummary/TimeSummary.js
@@ -0,0 +1,174 @@
+const asyncHandler = require("express-async-handler");
+const Project = require("../../../Modals/ProjectModel");
+const Company = require("../../../Modals/CompanySchema");
+const User = require("../../../Modals/userSchema");
+const TimeSheet = require("../../../Modals/TimeSheetModel");
+const { StatusCodes } = require("http-status-codes");
+const Employee = require("../../../Modals/EmployeeRegistrationModel");
+
+const TimeSummary = {
+    TotalHoursByResource: asyncHandler(async (req, res) => {
+        try {
+            console.log("userdd", req.user);
+            const user = await User.findById(req.user);
+            if (!user) {
+                res.status(StatusCodes.UNAUTHORIZED);
+                throw new Error("Unauthorized User. Please Sign up.");
+            }
+
+
+            const company = await Company.findOne({ UserId: user?.user_id });
+
+            if (!company) {
+                res.status(StatusCodes.BAD_REQUEST);
+                throw new Error("Company does not exist. Please create a company first.");
+            }
+            Employees = await Employee.find({ company_name: company?.Company_Id });
+            TimeSheetData = await TimeSheet.find({ company: company?.Company_Id });
+
+            const result = await Promise.all(Employees.map(async (employee) => {
+                if (!employee.id) {
+                    return null;
+                }
+
+                const employeeId = Number(employee.id);
+
+                const employeeTimesheets = TimeSheetData.filter(sheet => Number(sheet.resource) === employeeId);
+                const total_hours = employeeTimesheets.reduce((acc, sheet) => acc + (sheet.hours || 0), 0);
+                const billed_hours = employeeTimesheets.reduce((acc, sheet) => acc + (sheet.billed_hours || 0), 0);
+                return {
+                    resource: {
+                        id: employee.id.toString(),
+                        name: `${employee.first_name} ${employee.last_name}`,
+                        resource_type: employee.role,
+                    },
+                    total_hours,
+                    billed_hours,
+                };
+            }
+            ));
+            return res.status(StatusCodes.OK).json({ success: true, result });
+
+        } catch (error) {
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR);
+            throw new Error(error?.message);
+        }
+    }),
+    HoursByProject: asyncHandler(async (req, res) => {
+        try {
+            console.log("userdd", req.user);
+            const user = await User.findById(req.user);
+            if (!user) {
+                res.status(StatusCodes.UNAUTHORIZED);
+                throw new Error("Unauthorized User. Please Sign up.");
+            }
+
+
+            const company = await Company.findOne({ UserId: user?.user_id });
+
+            if (!company) {
+                res.status(StatusCodes.BAD_REQUEST);
+                throw new Error("Company does not exist. Please create a company first.");
+            }
+            const projects = await Project.find({ Company_Id: company?.Company_Id });
+            const timesheets = await TimeSheet.find({ company: company?.Company_Id });
+            console.log(projects, "projects");
+            
+            const projectNames = await Promise.all(projects.map(async (project) => {
+                const projectTimesheets = timesheets.filter(sheet => sheet.project === project?.Project_Id);
+                const totalHours = projectTimesheets.reduce((acc, sheet) => acc + (sheet.hours || 0), 0);
+                const billedHours = projectTimesheets.reduce((acc, sheet) => acc + (sheet.billed_hours || 0), 0);
+                return {
+                    id: project?.Project_Id,
+                    Project_Name: project?.Project_Name,
+                    Start_Date: project?.Start_Date,
+                    End_Date: project?.End_Date,
+                    totalHours,
+                    billedHours,
+                };
+            }
+            ));
+            return res.status(StatusCodes.OK).json({ success: true, projectNames });
+
+        } catch (error) {
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR);
+            throw new Error(error?.message);
+        }
+    }),
+    HoursByCompany: asyncHandler(async (req, res) => {
+        try {
+            console.log("userdd", req.user);
+            const user = await User.findById(req.user);
+            if (!user) {
+                res.status(StatusCodes.UNAUTHORIZED);
+                throw new Error("Unauthorized User. Please Sign up.");
+            }
+
+
+            const company = await Company.findOne({ UserId: user?.user_id });
+
+            if (!company) {
+                res.status(StatusCodes.BAD_REQUEST);
+                throw new Error("Company does not exist. Please create a company first.");
+            }
+            const timesheets = await TimeSheet.find({ company: company?.Company_Id });
+            const totalHours = timesheets.reduce((acc, sheet) => acc + (sheet.hours || 0), 0);
+            const billedHours = timesheets.reduce((acc, sheet) => acc + (sheet.billed_hours || 0), 0);
+            const ok_hours = timesheets.reduce((acc, sheet) => acc + (sheet.ok_hours || 0), 0);
+            const blank_hours = timesheets.reduce((acc, sheet) => acc + (sheet.blank_hours || 0), 0);  
+
+            return res.status(StatusCodes.OK).json({ success: true, company: company?.Company_Name, totalHours, billedHours, ok_hours, blank_hours });
+        } catch (error) {
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR);
+            throw new Error(error?.message);
+        }
+    }),
+   
+    ProjectTimeUtilization: asyncHandler(async (req, res) => {  
+        try {
+            console.log("userdd", req.user);
+            const user = await User.findById(req.user);
+            if (!user) {
+                res.status(StatusCodes.UNAUTHORIZED);
+                throw new Error("Unauthorized User. Please Sign up.");
+            }
+
+
+            const company = await Company.findOne({ UserId: user?.user_id });
+
+            if (!company) {
+                res.status(StatusCodes.BAD_REQUEST);
+                throw new Error("Company does not exist. Please create a company first.");
+            }
+            const projects = await Project.find({ Company_Id: company?.Company_Id });
+            const timesheets = await TimeSheet.find({ company: company?.Company_Id });
+            console.log(projects, "projects");
+            const projectNames = await Promise.all(projects.map(async (project) => {
+                const projectTimesheets = timesheets.filter(sheet => sheet.project === project?.Project_Id);
+                const totalHours = projectTimesheets.reduce((acc, sheet) => acc + (sheet.hours || 0), 0);
+                const billedHours = projectTimesheets.reduce((acc, sheet) => acc + (sheet.billed_hours || 0), 0);
+                const ok_hours = projectTimesheets.reduce((acc, sheet) => acc + (sheet.ok_hours || 0), 0);
+                const blank_hours = projectTimesheets.reduce((acc, sheet) => acc + (sheet.blank_hours || 0), 0);
+                const productivity = totalHours > 0 ? Math.round((billedHours / totalHours) * 100 * 100) / 100 : 0;
+                return {
+                    id: project?.Project_Id,
+                    Project_Name: project?.Project_Name,
+                    Start_Date: project?.Start_Date,
+                    End_Date: project?.End_Date,
+                    totalHours,
+                    billedHours,
+                    ok_hours,
+                    blank_hours,
+                    productivity,
+                };
+            }
+            ));
+            return res.status(StatusCodes.OK).json({ success: true, projectNames });
+        } catch (error) {
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR);
+            throw new Error(error?.message);
+        }
+    })
+};
+
+module.exports = TimeSummary;
--- Router/Admin/index.js
+++ Router/Admin/index.js
@@ -5,6 +5,10 @@ const verifyToken = require("../../Auth/VerifyAuth");
 const projectController = require("../../Controller/AdminController/ProjectController");
 const adminCtr = require("../../Controller/AdminController/Admincontroller");
 const timesheetController = require("../../Controller/AdminController/TimeSheetCtr");
+const chartscontroller = require("../../Controller/AdminController/ChartsController");
+const employeeController = require("../../Controller/AdminController/EmployeeCotroller");
+const ProductivityChart = require("../../Controller/AdminController/Productivity_LeaderBoard/Productivity_Leaderboard");
+const TimeSummary = require("../../Controller/AdminController/TimeSummary/TimeSummary");
 const adminRouter = express.Router();
 
 // admin ctr
@@ -30,5 +34,16 @@ adminRouter.post("/add-timesheet", timesheetController.createtimesheet);
 // employee ctr
 // adminRouter.get("/get-employee", verifyToken);
 // employee ctr
+
+adminRouter.get("/get-project-charts", verifyToken, chartscontroller?.ProjectCharts);
+adminRouter.get("/get-employee-time-hours", verifyToken, chartscontroller?.EmployeeTimeHours);
+adminRouter.get("/get-productivity-chart", verifyToken, ProductivityChart?.ProductivityChart);
+
+adminRouter.get("/get-total-hours-by-resource", verifyToken, TimeSummary?.TotalHoursByResource);
+adminRouter.get("/get-hours-by-project", verifyToken, TimeSummary?.HoursByProject);
+adminRouter.get("/get-hours-by-company", verifyToken, TimeSummary?.HoursByCompany);
+adminRouter.get("/get-project-time-utilization", verifyToken, TimeSummary?.ProjectTimeUtilization);
+
+adminRouter.post("/add-employee", verifyToken, employeeController?.createemployee);
 
 module.exports = adminRouter;
