16137747bab30cddfe9fae4f5fe8e79e50f3bddf
Vikaskumar8085 <vikaskumar7049002404@gmail.com>
2024-09-26 19:06:46 +0530

    Update code

--- Controller/AdminController/ChartsController.js
+++ Controller/AdminController/ChartsController.js
@@ -24,21 +24,27 @@ const chartscontroller = {
         throw new Error("company not exists please create first company");
       }
       //
-      const recentProjects = await Project.find({ CompanyId: company?.Company_Id })
-        .sort({ createdAt: -1 }) // Sort by creation date descending
+      const recentProjects = await Project.find({
+        CompanyId: company?.Company_Id,
+      })
+        .sort({createdAt: -1}) // Sort by creation date descending
         .limit(10);
 
+      // console.log(recentProjects);
 
       //we need only names of project and it's start date and end date
-      const projectNames = recentProjects?.map((project) => {
-        return {
-          id: project?.ProjectId,
-          Project_Name: project?.Project_Name,
-          Start_Date: project?.Start_Date,
-          End_Date: project?.End_Date,
-        };
+      // const projectNames = recentProjects?.map((project) => {
+      //   return {
+      //     id: project?.ProjectId,
+      //     Project_Name: project?.Project_Name,
+      //     Start_Date: project?.Start_Date,
+      //     End_Date: project?.End_Date,
+      //   };
+      // });
+      return res.status(StatusCodes.OK).json({
+        success: true,
+        result: recentProjects,
       });
-      return res.status(StatusCodes.OK).json({success: true, projectNames});
     } catch (error) {
       throw new Error(error?.message);
     }
@@ -57,10 +63,10 @@ const chartscontroller = {
         res.status(StatusCodes?.BAD_REQUEST);
         throw new Error("company not exists please create first company");
       }
-      const timesheet = await TimeSheet.find({ CompanId: company?.Company_Id});
+      const timesheet = await TimeSheet.find({CompanId: company?.Company_Id});
       const empName = async (EmployeeId) => {
         if (!EmployeeId) return null; // Handle case where id is not provided
-        const emp = await Employee.findOne({ EmployeeId }); // Query by the custom `id` field
+        const emp = await Employee.findOne({EmployeeId}); // Query by the custom `id` field
         return emp ? `${emp.FirstName} ${emp.LastName}` : null; // Return the name if employee is found
       };
 
